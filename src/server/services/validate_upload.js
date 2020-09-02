9a1dfd776cb58f6947096854fe3b3cfe520e7443
John McLaughlin <john.mclaughlin@originate.com>
2020-09-02 08:44:58 -0700

    fixes for filename validation

--- src/server/services/validate_upload.js
+++ src/server/services/validate_upload.js
@@ -1,11 +1,69 @@
 const { ValidationItem } = require("../lib/validation_log");
 
 const validateFilename = filename => {
-  if (/\d{3,}-\d{8}-\w+-v\d+\.xlsx?/.test(filename)) return null;
+  const valog = [];
+  const [, name, ext] = filename.match(/^(.*)\.([^.]+)$/) || [];
+  if (ext !== "xlsx") {
+    valog.push(
+      new ValidationItem({
+        message: `Uploaded file must have ".xlsx" extension`
+      })
+    );
+  }
+  const nameParts = (name || "").split("_");
 
-  return new ValidationItem({
-    message: `Uploaded file name must match pattern NNN-YYYYMMDD-short_name-vN.xlsx (ex. 013-20200523-ppe-v2.xlsx)`
-  });
+  const agency = nameParts.shift();
+  // TODO: specific rules for agency abbreviation
+  if (!agency) {
+    valog.push(
+      new ValidationItem({
+        message: `First part of file name must be an agency abbreviation.`
+      })
+    );
+  }
+
+  const projectId = nameParts.shift();
+  // TODO: specific rules for project id
+  if (!projectId) {
+    valog.push(
+      new ValidationItem({
+        message: `Second part of file name must be a project id.`
+      })
+    );
+  }
+
+  const version = nameParts.pop();
+  console.log("version", version);
+  if (!/^v\d+/.test(version)) {
+    valog.push(
+      new ValidationItem({
+        message: `Last part of filename must be a version number (e.g. _v3)`
+      })
+    );
+  }
+
+  const reportingDate = nameParts.pop();
+  // TODO: match against expected reporting date from config instead of hard coded.
+  const expectedEndReportDate = "06302020";
+  if (reportingDate !== expectedEndReportDate) {
+    valog.push(
+      new ValidationItem({
+        message: `Second to last part of filename must be the end report date (${expectedEndReportDate})`
+      })
+    );
+  }
+
+  if (valog.length) {
+    valog.push(
+      new ValidationItem({
+        message: `Uploaded file name must match pattern
+      <agency abbrev>_<project id>_<short project name>_<reporting due date>_v<version number>.xlsx
+      Example: EOH_Covid-Supp-013_ppe_06302020_v1.xlsx
+      `
+      })
+    );
+  }
+  return valog;
 };
 
 module.exports = {
--- tests/server/services/validate_upload.spec.js
+++ tests/server/services/validate_upload.spec.js
@@ -3,20 +3,21 @@ const expect = require("chai").expect;
 
 describe("lib/validate_upload", () => {
   describe("validateFilename", () => {
-    const goodFilename = "045-20200719-good-v3.xlsx";
+    const goodFilename = "EOH_Covid-Supp-013_ppe_06302020_v1.xlsx";
     const badFilenames = [
-      ["wrong extension", "045-20200719-bad-v3.csv"],
-      ["missing version", "045-20200719-bad.xlsx"],
-      ["bad date", "045-2020719-bad-v3.xlsx"]
+      ["wrong extension", "EOH_Covid-Supp-013_ppe_06302020_v1.csv"],
+      ["missing version", "EOH_Covid-Supp-013_ppe_06302020.xlsx"],
+      ["wrong date", "EOH_Covid-Supp-013_ppe_06312020.xlsx"]
     ];
     it("passes a good filename", () => {
       const result = validateFilename(goodFilename);
-      expect(result).to.be.null;
+      expect(result).to.be.empty;
     });
     badFilenames.forEach(([message, badFilename]) => {
       it(`${message} fails`, () => {
         const result = validateFilename(badFilename);
-        expect(result.info.message).to.match(
+        console.log("result", result);
+        expect(result[result.length - 1].info.message).to.match(
           /^Uploaded file name must match pattern/
         );
       });
