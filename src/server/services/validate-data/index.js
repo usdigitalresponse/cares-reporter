e9a07aafa45b741072ee35cff2c67c1d9a8c555f
Larry Hitchon <hitch@yondercloud.com>
2020-09-22 18:38:48 -0700

    add validations for cover tab

--- src/server/services/validate-data/index.js
+++ src/server/services/validate-data/index.js
@@ -1,12 +1,16 @@
 const _ = require("lodash");
 const validateSubrecipients = require("./subrecipients");
 const validateContracts = require("./contracts");
+const validateCover = require("./cover");
 const { getSubrecipientsHash } = require("./helpers");
 
 const validateData = (documents, fileParts) => {
   const valog = [];
   const groupedDocuments = _.groupBy(documents, "type");
   const subrecipientsHash = getSubrecipientsHash(groupedDocuments.subrecipient);
+
+  const coverValog = validateCover(groupedDocuments.cover, fileParts);
+  valog.push(...coverValog);
 
   const subrecipientValog = validateSubrecipients(
     groupedDocuments.subrecipient
