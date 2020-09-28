c41b7d19c46be9f42ddb9e2d6fba0c0d44749a54
Larry Hitchon <hitch@yondercloud.com>
2020-09-27 17:42:12 -0700

    convert emails to lower case before database reads and writes

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { requireAdminUser } = require("../access-helpers");
 const { createUser } = require("../db");
-const _ = require("lodash");
 
 router.post("/", requireAdminUser, function(req, res, next) {
   console.log("POST /users");
   console.log(req.body);
-  const user = _.pick(req.body, ["email", "role", "agency_id"]);
+  const { email, role, agency_id } = req.body;
+  if (!email) {
+    res.status(400).send("User email is required");
+    return;
+  }
+  const user = {
+    email: req.body.email.toLowerCase(),
+    role,
+    agency_id
+  };
   createUser(user)
     .then(result => res.json({ user: result }))
     .catch(e => {
