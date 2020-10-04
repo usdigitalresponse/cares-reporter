9dc5926de4cf3c95595abb97d6d69389c1b2aae4
Larry Hitchon <hitch@yondercloud.com>
2020-10-04 15:22:09 -0700

    one more message cleanup

--- src/server/services/validate-data/grants.js
+++ src/server/services/validate-data/grants.js
@@ -43,7 +43,7 @@ const requiredFields = [
   [
     "project id",
     matchesFilePart("projectId"),
-    `The "project id" in the file name does not match grant's "project id"`
+    `The "project id" in the file name does not match the grant's "project id"`
   ],
   [
     "subrecipient id",
