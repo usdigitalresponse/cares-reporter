667e67839d65a12c2615ed84d3804f83df9d8546
Larry Hitchon <hitch@yondercloud.com>
2020-10-25 05:34:21 -0700

    make agency dashboard more like admin dashboard

--- src/components/AgencyHome.vue
+++ src/components/AgencyHome.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
-    <div>
-      <h2>Dashboard</h2>
-      <h3 class="mt-3">Upload</h3>
-      <div>
+    <div class="row" v-if="currentReportingPeriod">
+      <div class="col-12">
+        <h3>
+          Reporting Period:
+          {{ dateFormat(currentReportingPeriod.start_date) }} to
+          {{ dateFormat(currentReportingPeriod.end_date) }}
+        </h3>
+      </div>
+    </div>
+    <div class="row buttons mt-5">
+      <div class="col-12">
         <button class="btn btn-primary" @click="startUpload">
           Upload Spreadsheet
         </button>
@@ -16,6 +23,7 @@
 
 <script>
 import UploadHistory from "../components/UploadHistory";
+import moment from "moment";
 import _ from "lodash";
 export default {
   name: "Home",
@@ -23,6 +31,9 @@ export default {
     UploadHistory
   },
   computed: {
+    currentReportingPeriod: function() {
+      return this.$store.getters.currentReportingPeriod;
+    },
     template: function() {
       return _.find(this.$store.state.configuration.templates, t =>
         t.name.match(/agency/i)
@@ -36,6 +47,11 @@ export default {
     startUpload(e) {
       e.preventDefault();
       this.$router.push({ path: "/new_upload" });
+    },
+    dateFormat: function(d) {
+      return moment(d)
+        .utc()
+        .format("MM-DD-YYYY");
     }
   }
 };
@@ -50,5 +66,8 @@ h2,
 td,
 pre {
   text-align: left;
+}
+.buttons {
+  text-align: center;
 }
 </style>
