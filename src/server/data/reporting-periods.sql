61bc34943dc0ea692a2aeebcdba2e8f631a369f9
Michael-Stanford <michael@stanford.cc>
2020-12-13 17:06:48 -0600

    fixed reporting-periods.sql

--- src/server/data/reporting-periods.sql
+++ src/server/data/reporting-periods.sql
@@ -4,7 +4,6 @@
 --
 
 COPY public.reporting_periods (id, name, start_date, end_date, period_of_performance_end_date, certified_at, certified_by, reporting_template, validation_rule_tags, open_date, close_date, review_period_start_date, review_period_end_date) FROM stdin;
-1	1	2020-03-01	2020-06-30	2020-12-30	\N	\N	\N	\N	2020-12-01	2020-12-01	2020-12-16	2020-12-23
 2	2	2020-03-01	2020-09-30	2020-12-30	\N	\N	\N	\N	2020-12-01	2020-12-01	2020-12-16	2020-12-23
 3	3	2020-10-01	2020-12-31	2020-12-30	\N	\N	\N	\N	2020-12-25	2021-01-11	2021-01-12	2021-01-20
 4	4	2021-01-01	2021-03-31	2020-12-30	\N	\N	\N	\N	2021-01-22	2021-04-12	2021-04-13	2021-04-20
