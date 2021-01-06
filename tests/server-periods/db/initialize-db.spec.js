895c198104bae10d6b7b59c5be90a96194c242f3
Michael-Stanford <michael@stanford.cc>
2021-01-06 15:07:52 -0600

    wip

--- tests/server-periods/db/initialize-db.spec.js
+++ tests/server-periods/db/initialize-db.spec.js
@@ -4,22 +4,13 @@
 --------------------------------------------------------------------------------
 
 */
-const fs = require('fs')
-const path = require('path')
-const { getPeriodSummaries, readSummaries } =
-  requireSrc(`${__dirname}/../../server/db`)
-const treasury = requireSrc(`${__dirname}/../../server/lib/treasury`)
-const _ = require('lodash')
-
-const reportingPeriods =
-  requireSrc(`${__dirname}/../../server/db/reporting-periods`)
-
-const expect = require('chai').expect
-const knex = requireSrc(`${__dirname}/../../server/db/connection`)
 const { execFileSync } = require('child_process')
 
 describe('Initialize the period 2 database', () => {
-  it('Initalize the period 2 database', async () => {
-    execFileSync('reset-db.sh')
+  it('Initalize the period 2 database', async function () {
+    this.timeout(5000)
+    let dbname = 'rptest2'
+    process.env.POSTGRES_URL = `postgres://localhost/${dbname}`
+    execFileSync('./tests/server-periods/reset-db.sh', [dbname], {stdio: 'inherit'})
   })
 })
--- tests/server-periods/reset-db.sh
+++ tests/server-periods/reset-db.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+# Default dbname is "rptest"
+dbname=${1:-rptest}
+echo Updating database ${dbname}
+
+echo POSTGRES_URL abc is $POSTGRES_URL
 # The actual directory of this file.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # set -x
 set -e
+echo DIR is ${DIR}
+FIXTURES_DIRECTORY="${DIR}/fixtures"
 
 # Import .env variables if not already defined.
-DOTENV="$DIR/../../.env"
+DOTENV="$(pwd)/.env"
 echo Getting environment from $DOTENV
 
 source /dev/stdin <<DONE
 $(grep -v '^#' $DOTENV | sed -E 's|^(.+)=(.*)|: ${\1=\2}; export \1|g')
 DONE
 
-# Default dbname is "rptest"
-dbname=${1:-rptest}
+echo environment database is $POSTGRES_URL
 
-echo Using database $dbname
 echo Upload directory is $UPLOAD_DIRECTORY
 echo Treasury directory is $TREASURY_DIRECTORY
 
-pwd
+echo PWD is $(pwd)
 
 mkdir -p $UPLOAD_DIRECTORY
 rm -rf $UPLOAD_DIRECTORY/*
 mkdir -p $TREASURY_DIRECTORY
 
-
-FIXTURES_DIRECTORY="./tests/server-periods/fixtures"
+echo readlink $(readlink) = $FIXTURES_DIRECTORY
 
 psql -h localhost -U postgres -w postgres -c "DROP DATABASE IF EXISTS $dbname"
 psql -h localhost -U postgres -w postgres -c "CREATE DATABASE $dbname"
@@ -36,4 +40,4 @@ psql -h localhost -U postgres -w postgres -c "CREATE DATABASE $dbname"
 # cp ${FIXTURES_DIRECTORY}/treasury-reports/* $TREASURY_DIRECTORY
 
 echo priming database
-psql $dbname postgres < ${FIXTURES_DIRECTORY}/${dbname}.sql >/dev/null
+# psql $dbname postgres < ${FIXTURES_DIRECTORY}/${dbname}.sql >/dev/null
