61d5e5e744177dd03220c6c77c05eb67c5e8d1da
Larry Hitchon <hitch@yondercloud.com>
2021-01-07 10:02:58 -0800

    make transfer tab validation for current quarter obligation match other tabs

--- src/server/services/validate-data/transfers.js
+++ src/server/services/validate-data/transfers.js
@@ -67,8 +67,8 @@ const requiredFields = [
   ],
   [
     "current quarter obligation",
-    numberIsLessThanOrEqual("award amount"),
-    "Current quarter obligation must be less than or equal to award amount"
+    whenNotBlank("current quanter obligation", numberIsLessThanOrEqual("award amount")),
+    "Transfer {{transfer number}} current quarter obligation {{current quarter obligation}} must be less than or equal to the award amount"
   ],
   [
     "award amount",
